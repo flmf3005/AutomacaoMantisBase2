659ded476545bb43039ae629645b0e9f3c3dd21e
Fernando Lucas <flmf3005@gmail.com>
2019-11-14 14:08:18 -0300

    Novos Testes Usuario

---
+++ AutomacaoAPIMantisBase2/Requests/UsuariosRequest.cs
@@ -0,0 +1,36 @@
+﻿using AutomacaoAPIMantisBase2.Bases;
+using AutomacaoAPIMantisBase2.Helpers;
+using RestSharp;
+using System.IO;
+using System.Text;
+
+namespace AutomacaoAPIMantisBase2.Requests
+{
+    public class UsuariosRequest : RequestBase
+    {
+        public UsuariosRequest()
+        {
+            requestService = "/api/rest/users/me";
+            method = Method.GET;
+            httpBasicAuthenticator = true;
+        }
+
+        public UsuariosRequest(Method metodo)
+        {
+            requestService = "/api/rest/users/";
+            method = metodo;
+            httpBasicAuthenticator = true;
+        }
+
+        public void setUserId(string user)
+        {
+            requestService = requestService + user;
+        }
+
+        public void setJsonBody(string usuario, string senha, string nome, string email)
+        {
+            jsonBody = File.ReadAllText(GeneralHelpers.ReturnProjectPath() + "Jsons/Usuario.json", Encoding.UTF8);
+            jsonBody = jsonBody.Replace("$usuario", usuario).Replace("$senha", senha).Replace("$nome", nome).Replace("$email", email);
+        }
+    }
+}
---
+++ AutomacaoAPIMantisBase2/Tests/UsuariosTests.cs
@@ -0,0 +1,78 @@
+﻿using AutomacaoAPIMantisBase2.Bases;
+using AutomacaoAPIMantisBase2.Requests;
+using NUnit.Framework;
+using RestSharp;
+
+namespace AutomacaoAPIMantisBase2.Tests
+{
+    public class UsuariosTests : TestBase
+    {
+        [Test]
+        public void BuscaDadosUsuarioAtual()
+        {
+            #region Parameters
+            string statusCodeEsperado = "OK";
+
+            //Resultado esperado
+
+            #endregion
+
+            UsuariosRequest request = new UsuariosRequest();
+
+            IRestResponse<dynamic> response = request.ExecuteRequest();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(statusCodeEsperado, response.StatusCode.ToString());
+            });
+        }
+
+        [Test]
+        public void InsereNovoUsuario()
+        {
+            #region Parameters
+            string statusCodeEsperado = "Created";
+
+            //Resultado esperado
+
+            #endregion
+
+            UsuariosRequest request = new UsuariosRequest(Method.POST);
+            request.setJsonBody("usertest", "usertest", "Usuario Teste", "user@gmail.com");
+
+            IRestResponse<dynamic> response = request.ExecuteRequest();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(statusCodeEsperado, response.StatusCode.ToString());
+            });
+        }
+
+        [Test]
+        public void DeletaUsuario()
+        {
+            #region Parameters
+            string statusCodeEsperado = "Created";
+            string idUser = null;
+            //Resultado esperado
+
+            #endregion
+
+            UsuariosRequest request = new UsuariosRequest(Method.POST);
+            request.setJsonBody("usertest", "usertest", "Usuario Teste", "delete@gmail.com");
+            IRestResponse<dynamic> response = request.ExecuteRequest();
+            idUser = response.Data["result"][0]["id"];
+
+            request = null;
+            request = new UsuariosRequest(Method.DELETE);
+            request.setUserId(idUser);
+
+            response = request.ExecuteRequest();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(statusCodeEsperado, response.StatusCode.ToString());
+            });
+        }
+    }
+}
