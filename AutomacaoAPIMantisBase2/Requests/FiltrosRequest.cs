8468391bf74abbb88f71b700d0b187883b5d7ef1
Fernando Lucas <flmf3005@gmail.com>
2019-11-14 12:47:35 -0300

    Novos Testes Filtros

---
+++ AutomacaoAPIMantisBase2/Requests/FiltrosRequest.cs
@@ -0,0 +1,30 @@
+﻿using AutomacaoAPIMantisBase2.Bases;
+using AutomacaoAPIMantisBase2.Helpers;
+using RestSharp;
+using System.IO;
+using System.Text;
+
+namespace AutomacaoAPIMantisBase2.Requests
+{
+    public class FiltrosRequest : RequestBase
+    {
+        public FiltrosRequest()
+        {
+            requestService = "/api/rest/filters/";
+            method = Method.GET;
+            httpBasicAuthenticator = true;
+        }
+
+        public FiltrosRequest(int filter, Method metodo)
+        {
+            requestService = "/api/rest/filters/" + filter;
+            method = metodo;
+            httpBasicAuthenticator = true;
+        }
+
+        public void setJsonBody()
+        {
+            
+        }
+    }
+}
---
+++ AutomacaoAPIMantisBase2/Tests/FiltrosTests.cs
@@ -0,0 +1,73 @@
+﻿using AutomacaoAPIMantisBase2.Bases;
+using AutomacaoAPIMantisBase2.Requests;
+using NUnit.Framework;
+using RestSharp;
+
+namespace AutomacaoAPIMantisBase2.Tests
+{
+    public class FiltrosTests : TestBase
+    {
+        [Test]
+        public void BuscaTodosOsFiltros()
+        {
+            #region Parameters
+            string statusCodeEsperado = "OK";
+
+            //Resultado esperado
+
+            #endregion
+
+            FiltrosRequest request = new FiltrosRequest();
+            request.setJsonBody();
+
+            IRestResponse<dynamic> response = request.ExecuteRequest();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(statusCodeEsperado, response.StatusCode.ToString());
+            });
+        }
+
+        [Test]
+        public void BuscaFiltroEspecifico()
+        {
+            #region Parameters
+            string statusCodeEsperado = "OK";
+
+            //Resultado esperado
+
+            #endregion
+
+            FiltrosRequest request = new FiltrosRequest(1, Method.GET);
+            request.setJsonBody();
+
+            IRestResponse<dynamic> response = request.ExecuteRequest();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(statusCodeEsperado, response.StatusCode.ToString());
+            });
+        }
+
+        [Test]
+        public void DeletaFiltroEspecifico()
+        {
+            #region Parameters
+            string statusCodeEsperado = "OK";
+
+            //Resultado esperado
+
+            #endregion
+
+            FiltrosRequest request = new FiltrosRequest(1, Method.DELETE);
+            request.setJsonBody();
+
+            IRestResponse<dynamic> response = request.ExecuteRequest();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(statusCodeEsperado, response.StatusCode.ToString());
+            });
+        }
+    }
+}
